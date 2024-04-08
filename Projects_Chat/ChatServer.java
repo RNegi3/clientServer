d1119d37345c7c4211097b48541cef47677a246f
RNegi3 <115223802+RNegi3@users.noreply.github.com>
2024-04-08 15:00:56 +0000

    Created package

---
+++ Projects_Chat/ChatServer.java
@@ -0,0 +1,14 @@
+
+package Projects_Chat;
+
+/**
+ * ChatServer
+ */
+public class ChatServer {
+    public void main(String[] args){
+        System.out.println("Hello world")
+    }
+
+    
+}
+    
